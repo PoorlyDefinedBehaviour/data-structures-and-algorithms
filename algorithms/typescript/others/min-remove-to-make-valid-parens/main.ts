d835eda70f62e58aebe098f7c18a8277e6cff2ed
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-01-17 15:35:03 -0300

    :sparkles: Add min remove to make a string valid

---
+++ algorithms/typescript/others/min-remove-to-make-valid-parens/main.ts
@@ -0,0 +1,67 @@
+/**
+ * time O(3n) -> O(n)
+ * space O(2n) -> O(n)
+ **/
+function minRemoveToMakeValid(s: string): string {
+  const indexesOfCharactersToRemove: number[] = [];
+  const letters = s.split("");
+
+  for (let i = 0; i < s.length; ++i) {
+    if (s[i] === "(") {
+      indexesOfCharactersToRemove.push(i);
+    } else if (s[i] === ")") {
+      if (indexesOfCharactersToRemove.length > 0) {
+        indexesOfCharactersToRemove.pop();
+      } else {
+        letters[i] = "";
+      }
+    }
+  }
+
+  for (const index of indexesOfCharactersToRemove) {
+    letters[index] = "";
+  }
+
+  return letters.join("");
+}
+
+function main() {
+  /* 
+Given a string s of '(' , ')' and lowercase English characters. 
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+    It is the empty string, contains only lowercase characters, or
+    It can be written as AB (A concatenated with B), where A and B are valid strings, or
+    It can be written as (A), where A is a valid string.
+
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+Example 4:
+
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+
+*/
+  console.log("lee(t(c)o)de)", minRemoveToMakeValid("lee(t(c)o)de)"));
+}
+main();
