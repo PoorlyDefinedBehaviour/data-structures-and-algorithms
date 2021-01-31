3b56b32a6c1d634c18d385f2d94399723878463d
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-01-31 19:43:11 -0300

    feat(clojure): add is string balanced

---
+++ clojure/algorithms/others/is-string-balanced/main.clj
@@ -0,0 +1,62 @@
+(ns kata.core)
+(use 'clojure.test)
+(require 'clojure.string)
+
+;; Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+;; An input string is valid if :
+;; Open brackets must be closed by the same type of brackets.Open brackets must be closed in the correct order.
+;; Note that an empty string is also considered valid.
+;; Examples:
+;; Input : "()" Output : true
+;; Input : "()[]{}" Output : true
+;; Input : "(]" Output : false
+
+(defn is-opening [character]
+  (case character
+    "(" true
+    "[" true
+    "{" true
+    false))
+
+(defn is-closing [character]
+  (case character
+    ")" true
+    "]" true
+    "}" true
+    false))
+
+(defn matches [character-a character-b]
+  (case character-b
+    ")" (= character-a "(")
+    "]" (= character-a "[")
+    "}" (= character-a "{")
+    false))
+
+(defn matches-last-character [characters character]
+  (if (empty? characters) false
+      (let [last-character (last characters)]
+        (matches last-character character))))
+
+(defn are-there-characters-remaining [result]
+  (if (vector? result)
+    (empty?  result)
+    result))
+
+(defn is-string-balanced [s]
+  (let [result (reduce (fn [characters character]
+                         (if (is-opening character)
+                           (conj characters character)
+                           (if (not (matches-last-character characters character))
+                             (reduced false)
+                             (vec (drop-last 1 characters)))))
+                       []
+                       (clojure.string/split s #""))]
+    (are-there-characters-remaining result)))
+
+(deftest is-string-balanced-tests
+  (is (= true (is-string-balanced "()")))
+  (is (= true (is-string-balanced "()[]{}")))
+  (is (= false (is-string-balanced "(]")))
+  (is (= false (is-string-balanced "()()()(()))"))))
+
+(run-tests)
