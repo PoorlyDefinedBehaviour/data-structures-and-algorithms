d9cd07102540ec6acd7603477b88e13ba313b6d5
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-06-06 19:06:46 -0300

    feat(go): add binary search tree

---
+++ golang/data_structures/binary_search_tree/main.go
@@ -0,0 +1,375 @@
+package main
+
+import (
+	"math"
+)
+
+// A binary tree is made of nodes, where each node contains
+// a left pointer, a right pointer, and a data element.
+// The root pointer points to the topmost node in the tree.
+// The left and right pointers recursively point to smaller subtrees
+// on either side. A null pointer represents a binary tree
+// with no elements -- the empty tree.
+// The formal recursive definition is: a binary tree is either
+// empty(represented by a null pointer), or is miade of a single node,
+// where the left and right pointers each point to a binary tree.
+//
+// type BinaryTree struct {
+// 	Data  int
+// 	Left  *BinaryTree
+// 	Right *BinaryTree
+// }
+//
+// A binary search tree is a type of binary tree where the nodes are arranged in order:
+// for each node, all elements in its left subtree ar less-or-equal to the node,
+// and all elements in its right subtree are greater than the node.
+// The nodes at the bottom edge of the tree have empty subtrees and are called
+// leaf nodes while the others are internal nodes.
+//
+// Binary search trees are fast at insert and lookup.
+// On average, a binary search tree algorithm can locate a node in
+// a N node tree in order log(n) time. Therefore, binary search trees
+// are good for dictionary problems where the code inserts and looks up
+// information indexed by some key.
+// The log(n) behaviour is the average case, it's possible for a particular
+// tree to be much slower if it is unbalanced.
+
+type BinarySearchTree struct {
+	Data  int
+	Left  *BinarySearchTree
+	Right *BinarySearchTree
+}
+
+func NewBinarySearchTree(data int) *BinarySearchTree {
+	return &BinarySearchTree{
+		Data:  data,
+		Left:  nil,
+		Right: nil,
+	}
+}
+
+func (tree *BinarySearchTree) IsLeaf() bool {
+	if tree == nil {
+		return false
+	}
+
+	return tree.Left == nil && tree.Right == nil
+}
+
+// time O(log n)
+// space O(log n) recursive calls without tail call optimization.
+func (tree *BinarySearchTree) Contains(target int) bool {
+	if tree == nil {
+		return false
+	}
+
+	if tree.Data == target {
+		return true
+	}
+
+	if target < tree.Data {
+		return tree.Left.Contains(target)
+	}
+
+	return tree.Right.Contains(target)
+}
+
+// time O(log n)
+// space O(1).
+func (tree *BinarySearchTree) ContainsIterative(target int) bool {
+	currentNode := tree
+
+	for currentNode != nil {
+		if currentNode.Data == target {
+			return true
+		}
+
+		if target < currentNode.Data {
+			currentNode = currentNode.Left
+		} else {
+			currentNode = currentNode.Right
+		}
+	}
+
+	return false
+}
+
+// time O(log n)
+// space O(log n).
+func (tree *BinarySearchTree) Insert(data int) *BinarySearchTree {
+	if tree == nil {
+		return NewBinarySearchTree(data)
+	}
+
+	if data <= tree.Data {
+		tree.Left = tree.Left.Insert(data)
+	} else {
+		tree.Right = tree.Right.Insert(data)
+	}
+
+	return tree
+}
+
+// time O(log n)
+// space O(1).
+func (tree *BinarySearchTree) InsertIterative(data int) *BinarySearchTree {
+	if tree == nil {
+		return NewBinarySearchTree(data)
+	}
+
+	currentNode := tree
+
+	for {
+		if data <= currentNode.Data {
+			if currentNode.Left == nil {
+				currentNode.Left = NewBinarySearchTree(data)
+				break
+			} else {
+				currentNode = currentNode.Left
+			}
+		} else {
+			if currentNode.Right == nil {
+				currentNode.Right = NewBinarySearchTree(data)
+				break
+			} else {
+				currentNode = currentNode.Right
+			}
+		}
+	}
+
+	return tree
+}
+
+// time O(n)
+// space O(n).
+func (tree *BinarySearchTree) CountNodes() int {
+	if tree == nil {
+		return 0
+	}
+
+	return 1 + tree.Left.CountNodes() + tree.Right.CountNodes()
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) Height() int {
+	if tree == nil {
+		return 0
+	}
+
+	return 1 + int(math.Max(float64(tree.Left.Height()), float64(tree.Right.Height())))
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) HeightIterative() int {
+	if tree == nil {
+		return 0
+	}
+
+	queue := []*BinarySearchTree{tree}
+
+	height := 0
+
+	for len(queue) > 0 {
+		height++
+
+		nodeCount := len(queue)
+
+		for nodeCount > 0 {
+			node := queue[0]
+			queue = queue[1:]
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+
+			nodeCount--
+		}
+	}
+
+	return height
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) InOrder(visitor func(tree *BinarySearchTree)) {
+	if tree == nil {
+		return
+	}
+
+	tree.Left.InOrder(visitor)
+
+	visitor(tree)
+
+	tree.Right.InOrder(visitor)
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) PostOrder(visitor func(tree *BinarySearchTree)) {
+	if tree == nil {
+		return
+	}
+
+	tree.Left.PostOrder(visitor)
+	tree.Right.PostOrder(visitor)
+
+	visitor(tree)
+}
+
+func (tree *BinarySearchTree) hasPathSum(target, currentSum int) bool {
+	if tree == nil {
+		return false
+	}
+
+	sum := currentSum + tree.Data
+
+	if tree.IsLeaf() {
+		return sum == target
+	}
+
+	return tree.Left.hasPathSum(target, sum) || tree.Right.hasPathSum(target, sum)
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) HasPathSum(target int) bool {
+	return tree.hasPathSum(target, 0)
+}
+
+func (tree *BinarySearchTree) leafPaths(paths *[][]int, currentPath []int) {
+	if tree == nil {
+		return
+	}
+
+	currentPath = append(currentPath, tree.Data)
+
+	if tree.IsLeaf() {
+		currentPathCopy := make([]int, len(currentPath))
+		copy(currentPathCopy, currentPath)
+
+		*paths = append(*paths, currentPathCopy)
+		return
+	}
+
+	tree.Left.leafPaths(paths, currentPath)
+	tree.Right.leafPaths(paths, currentPath)
+}
+
+// time O(n)
+// space O(n * leafs)
+func (tree *BinarySearchTree) LeafPaths() [][]int {
+	paths := make([][]int, 0)
+
+	currentPath := make([]int, 0)
+
+	tree.leafPaths(&paths, currentPath)
+
+	return paths
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) NodeMap(f func(*BinarySearchTree) *BinarySearchTree) *BinarySearchTree {
+	if tree == nil {
+		return nil
+	}
+
+	newTree := f(tree)
+	newTree.Left = newTree.Left.NodeMap(f)
+	newTree.Right = newTree.Right.NodeMap(f)
+
+	return newTree
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) Map(f func(int) int) *BinarySearchTree {
+	return tree.NodeMap(func(tree *BinarySearchTree) *BinarySearchTree {
+		return &BinarySearchTree{
+			Data:  f(tree.Data),
+			Left:  tree.Left,
+			Right: tree.Right,
+		}
+	})
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) Fold(initialValue interface{}, f func(interface{}, interface{}, interface{}) interface{}) interface{} {
+	if tree == nil {
+		return initialValue
+	}
+
+	return f(tree.Data, tree.Left.Fold(initialValue, f), tree.Right.Fold(initialValue, f))
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) Mirror() *BinarySearchTree {
+	return tree.NodeMap(func(tree *BinarySearchTree) *BinarySearchTree {
+		return &BinarySearchTree{
+			Data:  tree.Data,
+			Left:  tree.Right,
+			Right: tree.Left,
+		}
+	})
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) DoubleTree() *BinarySearchTree {
+	if tree == nil {
+		return nil
+	}
+
+	return &BinarySearchTree{
+		Data: tree.Data,
+		Left: &BinarySearchTree{
+			Data:  tree.Data,
+			Left:  tree.Left.DoubleTree(),
+			Right: nil,
+		},
+		Right: tree.Right.DoubleTree(),
+	}
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) IsEqual(other *BinarySearchTree) bool {
+	if tree == nil || other == nil {
+		return tree == other
+	}
+
+	if tree.Data != other.Data {
+		return false
+	}
+
+	return tree.Left.IsEqual(other.Left) && tree.Right.IsEqual(other.Right)
+}
+
+// time O(n)
+// space O(n)
+func (tree *BinarySearchTree) IsBalanced() bool {
+	if tree == nil {
+		return true
+	}
+
+	if tree.Left != nil && tree.Left.Data > tree.Data {
+		return false
+	}
+
+	if tree.Right != nil && tree.Right.Data < tree.Data {
+		return false
+	}
+
+	return tree.Left.IsBalanced() && tree.Right.IsBalanced()
+}
+
+func main() {
+}
---
+++ golang/data_structures/binary_search_tree/main_test.go
@@ -0,0 +1,788 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinarySearchTree_IsLeaf(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected bool
+	}{
+		{
+			tree:     nil,
+			expected: false,
+		},
+		{
+			tree:     NewBinarySearchTree(3),
+			expected: true,
+		},
+		{
+			tree:     NewBinarySearchTree(3).Insert(2).Insert(4),
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.tree.IsLeaf()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_Contains(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		target   int
+		expected bool
+	}{
+		{
+			tree:     nil,
+			target:   10,
+			expected: false,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data:  10,
+				Left:  nil,
+				Right: nil,
+			},
+			target:   48,
+			expected: false,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data:  10,
+				Left:  nil,
+				Right: nil,
+			},
+			target:   10,
+			expected: true,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 0,
+				Left: &BinarySearchTree{
+					Data:  -1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  1,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			target:   -1,
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		actualRecursive := tt.tree.Contains(tt.target)
+		actualIterative := tt.tree.ContainsIterative(tt.target)
+
+		assert.Equal(t, tt.expected, actualRecursive)
+		assert.Equal(t, tt.expected, actualIterative)
+	}
+}
+
+func TestBinarySearchTree_Insert(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		recursiveInsertTree *BinarySearchTree
+		iterativeInsertTree *BinarySearchTree
+		expected            *BinarySearchTree
+	}{
+		{
+			recursiveInsertTree: NewBinarySearchTree(32).Insert(12),
+			iterativeInsertTree: NewBinarySearchTree(32).InsertIterative(12),
+			expected: &BinarySearchTree{
+				Data: 32,
+				Left: &BinarySearchTree{
+					Data:  12,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+		},
+		{
+			recursiveInsertTree: NewBinarySearchTree(32).Insert(42),
+			iterativeInsertTree: NewBinarySearchTree(32).InsertIterative(42),
+			expected: &BinarySearchTree{
+				Data: 32,
+				Left: nil,
+				Right: &BinarySearchTree{
+					Data:  42,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		},
+		{
+			recursiveInsertTree: NewBinarySearchTree(0).Insert(1).Insert(2),
+			iterativeInsertTree: NewBinarySearchTree(0).InsertIterative(1).InsertIterative(2),
+			expected: &BinarySearchTree{
+				Data: 0,
+				Left: nil,
+				Right: &BinarySearchTree{
+					Data: 1,
+					Left: nil,
+					Right: &BinarySearchTree{
+						Data:  2,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+		},
+		{
+			recursiveInsertTree: NewBinarySearchTree(0).Insert(-10).Insert(10),
+			iterativeInsertTree: NewBinarySearchTree(0).InsertIterative(-10).InsertIterative(10),
+			expected: &BinarySearchTree{
+				Data: 0,
+				Left: &BinarySearchTree{
+					Data:  -10,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  10,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		},
+		{
+			recursiveInsertTree: NewBinarySearchTree(0).Insert(-5).Insert(5).Insert(-10).Insert(10).Insert(30),
+			iterativeInsertTree: NewBinarySearchTree(0).InsertIterative(-5).InsertIterative(5).InsertIterative(-10).InsertIterative(10).InsertIterative(30),
+			expected: &BinarySearchTree{
+				Data: 0,
+				Left: &BinarySearchTree{
+					Data: -5,
+					Left: &BinarySearchTree{
+						Data:  -10,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data: 5,
+					Left: nil,
+					Right: &BinarySearchTree{
+						Data: 10,
+						Left: nil,
+						Right: &BinarySearchTree{
+							Data:  30,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, tt.recursiveInsertTree)
+		assert.Equal(t, tt.expected, tt.iterativeInsertTree)
+	}
+}
+
+func TestBinarySearchTree_CountNodes(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected int
+	}{
+		{
+			tree:     nil,
+			expected: 0,
+		},
+		{
+			tree:     NewBinarySearchTree(100),
+			expected: 1,
+		},
+		{
+			tree:     NewBinarySearchTree(0).Insert(-1).Insert(2),
+			expected: 3,
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.tree.CountNodes()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_Height(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected int
+	}{
+		{
+			tree:     nil,
+			expected: 0,
+		},
+		{
+			tree:     NewBinarySearchTree(100),
+			expected: 1,
+		},
+		{
+			tree:     NewBinarySearchTree(0).Insert(-1).Insert(2).Insert(3).Insert(-2),
+			expected: 3,
+		},
+		{
+			tree:     NewBinarySearchTree(4).Insert(3).Insert(2).Insert(1).Insert(0),
+			expected: 5,
+		},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, tt.tree.Height())
+		assert.Equal(t, tt.expected, tt.tree.HeightIterative())
+	}
+}
+
+func TestBinarySearchTree_InOrder(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected []int
+	}{
+		{
+			tree:     nil,
+			expected: make([]int, 0),
+		},
+		{
+			tree:     NewBinarySearchTree(4).Insert(2).Insert(5).Insert(1).Insert(3),
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		numbers := make([]int, 0)
+
+		tt.tree.InOrder(func(tree *BinarySearchTree) {
+			numbers = append(numbers, tree.Data)
+		})
+
+		assert.Equal(t, tt.expected, numbers)
+	}
+}
+
+func TestBinarySearchTree_PostOrder(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected []int
+	}{
+		{
+			tree:     nil,
+			expected: make([]int, 0),
+		},
+		{
+			tree:     NewBinarySearchTree(4).Insert(2).Insert(5).Insert(1).Insert(3),
+			expected: []int{1, 3, 2, 5, 4},
+		},
+	}
+	for _, tt := range tests {
+		numbers := make([]int, 0)
+
+		tt.tree.PostOrder(func(tree *BinarySearchTree) {
+			numbers = append(numbers, tree.Data)
+		})
+
+		assert.Equal(t, tt.expected, numbers)
+	}
+}
+
+func TestBinarySearchTree_HasPathSum(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		target   int
+		expected bool
+	}{
+		{
+			tree:     nil,
+			target:   27,
+			expected: false,
+		},
+		{
+			tree:     NewBinarySearchTree(3),
+			target:   0,
+			expected: false,
+		},
+		{
+			tree:     NewBinarySearchTree(3),
+			target:   3,
+			expected: true,
+		},
+		{
+			tree: NewBinarySearchTree(1).Insert(2).Insert(3).
+				Insert(-10).Insert(0).Insert(-5),
+			target:   6,
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.tree.HasPathSum(tt.target)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_LeafPaths(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected [][]int
+	}{
+		{
+			tree:     nil,
+			expected: make([][]int, 0),
+		},
+		{
+			tree: &BinarySearchTree{
+				Data:  0,
+				Left:  nil,
+				Right: nil,
+			},
+			expected: [][]int{
+				{0},
+			},
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 0,
+				Left: &BinarySearchTree{
+					Data: -5,
+					Left: &BinarySearchTree{
+						Data: -10,
+						Left: &BinarySearchTree{
+							Data:  -8,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &BinarySearchTree{
+							Data:  -15,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data: 5,
+					Left: &BinarySearchTree{
+						Data:  4,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &BinarySearchTree{
+						Data:  10,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+			expected: [][]int{
+				{0, -5, -10, -8},
+				{0, -5, -10, -15},
+				{0, 5, 4},
+				{0, 5, 10},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.tree.LeafPaths()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_Mirror(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected *BinarySearchTree
+	}{
+		{
+			tree:     nil,
+			expected: nil,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 4,
+				Left: &BinarySearchTree{
+					Data: 2,
+					Left: &BinarySearchTree{
+						Data:  1,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &BinarySearchTree{
+						Data:  3,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+				Right: &BinarySearchTree{
+					Data:  5,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			expected: &BinarySearchTree{
+				Data: 4,
+				Left: &BinarySearchTree{
+					Data:  5,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data: 2,
+					Left: &BinarySearchTree{
+						Data:  3,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &BinarySearchTree{
+						Data:  1,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.tree.Mirror()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_NodeMap(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		f        func(*BinarySearchTree) *BinarySearchTree
+		expected *BinarySearchTree
+	}{
+		{
+			tree: nil,
+			f: func(tree *BinarySearchTree) *BinarySearchTree {
+				panic("oops")
+			},
+			expected: nil,
+		},
+		{
+			tree: NewBinarySearchTree(2).Insert(1).Insert(3),
+			f: func(tree *BinarySearchTree) *BinarySearchTree {
+				return &BinarySearchTree{
+					Data:  tree.Data * 2,
+					Left:  tree.Left,
+					Right: tree.Right,
+				}
+			},
+			expected: NewBinarySearchTree(4).Insert(2).Insert(6),
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.tree.NodeMap(tt.f)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_Map(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		f        func(int) int
+		expected *BinarySearchTree
+	}{
+		{
+			tree: nil,
+			f: func(data int) int {
+				panic("oops")
+			},
+			expected: nil,
+		},
+		{
+			tree: NewBinarySearchTree(2).Insert(1).Insert(3),
+			f: func(data int) int {
+				return data * 2
+			},
+			expected: NewBinarySearchTree(4).Insert(2).Insert(6),
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.tree.Map(tt.f)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_DoubleTree(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected *BinarySearchTree
+	}{
+		{
+			tree:     nil,
+			expected: nil,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 2,
+				Left: &BinarySearchTree{
+					Data:  1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			expected: &BinarySearchTree{
+				Data: 2,
+				Left: &BinarySearchTree{
+					Data: 2,
+					Left: &BinarySearchTree{
+						Data: 1,
+						Left: &BinarySearchTree{
+							Data:  1,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: nil,
+					},
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data: 3,
+					Left: &BinarySearchTree{
+						Data:  3,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: nil,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.tree.DoubleTree()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_IsEqual(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		a        *BinarySearchTree
+		b        *BinarySearchTree
+		expected bool
+	}{
+		{
+			a:        nil,
+			b:        nil,
+			expected: true,
+		},
+		{
+			a:        nil,
+			b:        NewBinarySearchTree(32),
+			expected: false,
+		},
+		{
+			a:        NewBinarySearchTree(32),
+			b:        nil,
+			expected: false,
+		},
+		{
+			a: &BinarySearchTree{
+				Data: 1,
+				Left: &BinarySearchTree{
+					Data:  1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+			b: &BinarySearchTree{
+				Data:  1,
+				Left:  nil,
+				Right: nil,
+			},
+			expected: false,
+		},
+		{
+			a: &BinarySearchTree{
+				Data:  1,
+				Left:  nil,
+				Right: nil,
+			},
+			b: &BinarySearchTree{
+				Data:  1,
+				Left:  nil,
+				Right: nil,
+			},
+			expected: true,
+		},
+		{
+			a: &BinarySearchTree{
+				Data: 1,
+				Left: &BinarySearchTree{
+					Data:  1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			b: &BinarySearchTree{
+				Data: 1,
+				Left: &BinarySearchTree{
+					Data:  1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.a.IsEqual(tt.b)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_IsBalanced(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree     *BinarySearchTree
+		expected bool
+	}{
+		{
+			tree:     nil,
+			expected: true,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data:  1,
+				Left:  nil,
+				Right: nil,
+			},
+			expected: true,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 1,
+				Left: &BinarySearchTree{
+					Data:  2,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			expected: false,
+		},
+		{
+			tree: &BinarySearchTree{
+				Data: 1,
+				Left: &BinarySearchTree{
+					Data:  -1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &BinarySearchTree{
+					Data:  0,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			expected: false,
+		},
+	}
+	for _, tt := range tests {
+		actual := tt.tree.IsBalanced()
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
+
+func TestBinarySearchTree_Fold(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		tree         *BinarySearchTree
+		initialValue interface{}
+		f            func(interface{}, interface{}, interface{}) interface{}
+		expected     int
+	}{
+		{
+			tree:         nil,
+			initialValue: 0,
+			f: func(interface{}, interface{}, interface{}) interface{} {
+				panic("oops")
+			},
+			expected: 0,
+		},
+		{
+			tree:         NewBinarySearchTree(2).Insert(1).Insert(3),
+			initialValue: 0,
+			f: func(accum interface{}, left interface{}, right interface{}) interface{} {
+				a := accum.(int)
+				l := left.(int)
+				r := right.(int)
+
+				return a + l + r
+			},
+			expected: 6,
+		},
+	}
+
+	for _, tt := range tests {
+		actual := tt.tree.Fold(tt.initialValue, tt.f)
+
+		assert.Equal(t, tt.expected, actual)
+	}
+}
