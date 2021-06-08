4996aec45964a4f4b435724291b0dccc605e9d83
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-06-07 22:36:40 -0300

    feat(go/counting bits): add explanation

--- golang/algorithms/others/counting_bits/main.go
+++ golang/algorithms/others/counting_bits/main.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
 Example 1:
 
 Input: n = 2
@@ -29,6 +31,16 @@ Constraints:
 func countBits(n int) []int {
 	dp := make([]int, n+1)
 
+	/*
+		example:
+		i = 2
+		2 in binary = 10
+		10 >> 1 = 01
+		01 = 1
+		1 + (10 & 01)
+		1 + (0)
+		1
+	*/
 	for i := 0; i <= n; i++ {
 		dp[i] = dp[i>>1] + (i & 1)
 	}
