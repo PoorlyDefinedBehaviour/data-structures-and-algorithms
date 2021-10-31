c160683558ff4675f9c1e2a42b9b54fb8b8673ad
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-10-31 14:10:28 -0300

    feat(go): trim a binary search tree

---
+++ golang/algorithms/others/trim_a_binary_search_tree/main.go
@@ -0,0 +1,60 @@
+package main
+
+// 669. Trim a Binary Search Tree
+//
+// Given the root of a binary search tree and
+// the lowest and highest boundaries as low and high,
+// trim the tree so that all its elements lies in [low, high].
+// Trimming the tree should not change the relative structure of
+// the elements that will remain in the tree
+// (i.e., any node's descendant should remain a descendant).
+// It can be proven that there is a unique answer.
+//
+// Return the root of the trimmed binary search tree.
+// Note that the root may change depending on the given bounds.
+//
+// Examples
+//
+// Input: root = [1,0,2], low = 1, high = 2
+// Output: [1,null,2]
+//
+// Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+// Output: [3,2,null,1]
+//
+// Input: root = [1], low = 1, high = 2
+// Output: [1]
+//
+// Input: root = [1,null,2], low = 1, high = 3
+// Output: [1,null,2]
+//
+// Input: root = [1,null,2], low = 2, high = 4
+// Output: [2]
+type TreeNode struct {
+	Value int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// time O(n) where n is the number of nodes in the tree
+// space O(h) where h is the height of the tree
+func (node *TreeNode) Trim(low int, high int) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	if node.Value < low {
+		return node.Right.Trim(low, high)
+	}
+
+	if node.Value > high {
+		return node.Left.Trim(low, high)
+	}
+
+	node.Left = node.Left.Trim(low, high)
+	node.Right = node.Right.Trim(low, high)
+
+	return node
+}
+
+func main() {
+}
---
+++ golang/algorithms/others/trim_a_binary_search_tree/main_test.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_TreeNode_Trim(t *testing.T) {
+	t.Parallel()
+
+	t.Run("example 1", func(t *testing.T) {
+		node := &TreeNode{
+			Value: 1,
+			Left: &TreeNode{
+				Value: 0,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Value: 2,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		expected := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: &TreeNode{
+				Value: 2,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		assert.Equal(t, expected, node.Trim(1, 2))
+	})
+
+	t.Run("example 2", func(t *testing.T) {
+		node := &TreeNode{
+			Value: 3,
+			Left: &TreeNode{
+				Value: 0,
+				Left:  nil,
+				Right: &TreeNode{
+					Value: 2,
+					Left: &TreeNode{
+						Value: 1,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Value: 4,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		expected := &TreeNode{
+			Value: 3,
+			Left: &TreeNode{
+				Value: 2,
+				Left: &TreeNode{
+					Value: 1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: nil,
+			},
+			Right: nil,
+		}
+
+		assert.Equal(t, expected, node.Trim(1, 3))
+	})
+
+	t.Run("example 3", func(t *testing.T) {
+		node := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: nil,
+		}
+
+		expected := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: nil,
+		}
+
+		assert.Equal(t, expected, node.Trim(1, 2))
+	})
+
+	t.Run("example 4", func(t *testing.T) {
+		node := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: &TreeNode{
+				Value: 2,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		expected := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: &TreeNode{
+				Value: 2,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		assert.Equal(t, expected, node.Trim(1, 3))
+	})
+
+	t.Run("example 5", func(t *testing.T) {
+		node := &TreeNode{
+			Value: 1,
+			Left:  nil,
+			Right: &TreeNode{
+				Value: 2,
+				Left:  nil,
+				Right: nil,
+			},
+		}
+
+		expected := &TreeNode{
+			Value: 2,
+			Left:  nil,
+			Right: nil,
+		}
+
+		assert.Equal(t, expected, node.Trim(2, 4))
+	})
+}
