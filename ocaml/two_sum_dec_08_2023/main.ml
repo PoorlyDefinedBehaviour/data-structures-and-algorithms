5bf6a2629d2c81e8c23e25a7ae85d07c63196b68
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-12-08 11:00:14 -0300

    ocaml: two sum

---
+++ ocaml/two_sum_dec_08_2023/main.ml
@@ -0,0 +1,59 @@
+(*
+   Given an array of integers nums and an integer target,
+      return indices of the two numbers such that they add up to target.
+
+      You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+      You can return the answer in any order.
+
+
+
+      Example 1:
+
+      Input: nums = [2,7,11,15], target = 9
+      Output: [0,1]
+      Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+      Example 2:
+
+      Input: nums = [3,2,4], target = 6
+      Output: [1,2]
+      Example 3:
+
+      Input: nums = [3,3], target = 6
+      Output: [0,1]
+
+
+      Constraints:
+
+      2 <= nums.length <= 104
+      -109 <= nums[i] <= 109
+      -109 <= target <= 109
+      Only one valid answer exists.
+
+
+      Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity? 
+*)
+
+(*
+   time O(n)
+   space O(n)
+*)
+let two_sum nums target =
+  let cache = Hashtbl.create 0 in
+
+  let rec go cache i nums target =
+    match nums with
+    | [] -> assert false
+    | num :: tail ->
+        let diff = abs (target - num) in
+        if Hashtbl.mem cache diff then [ Hashtbl.find cache diff; i ]
+        else (
+          Hashtbl.replace cache num i;
+          (go [@tailcall]) cache (i + 1) tail target)
+  in
+  go cache 0 nums target
+
+let () =
+  Printf.printf "example 1: %b\n" ([ 0; 1 ] = two_sum [ 2; 7; 11; 15 ] 9);
+  Printf.printf "example 2: %b\n" ([ 1; 2 ] = two_sum [ 3; 2; 4 ] 6);
+  Printf.printf "example 3: %b\n" ([ 0; 1 ] = two_sum [ 3; 3 ] 6)
