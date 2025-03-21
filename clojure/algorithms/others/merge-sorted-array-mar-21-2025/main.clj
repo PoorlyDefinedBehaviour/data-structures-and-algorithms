9dd534b659accc2a56aead357d2ea7f9ed80a267
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-03-21 17:05:16 -0300

    clojure: merge sorted array

---
+++ clojure/algorithms/others/merge-sorted-array-mar-21-2025/main.clj
@@ -0,0 +1,60 @@
+(ns mini.playground
+  (:require
+   [clojure.test :refer [deftest is testing]]))
+
+;; You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+;; and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+;; Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+;; The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+;; Example 1:
+
+;; Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+;; Output: [1,2,2,3,5,6]
+;; Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+;; The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+;; Example 2:
+
+;; Input: nums1 = [1], m = 1, nums2 = [], n = 0
+;; Output: [1]
+;; Explanation: The arrays we are merging are [1] and [].
+;; The result of the merge is [1].
+;; Example 3:
+
+;; Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+;; Output: [1]
+;; Explanation: The arrays we are merging are [] and [1].
+;; The result of the merge is [1].
+;; Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+;; Constraints:
+
+;; nums1.length == m + n
+;; nums2.length == n
+;; 0 <= m, n <= 200
+;; 1 <= m + n <= 200
+;; -109 <= nums1[i], nums2[j] <= 109
+
+(defn merge [nums1 nums2]
+  (loop [result []
+         nums1 nums1
+         nums2 nums2]
+    (cond
+      (and (empty? nums1) (empty? nums2)) result
+      (empty? nums1) (concat result nums2)
+      (empty? nums2) (concat result nums1)
+      :else (if (<= (first nums1) (first nums2))
+              (recur (conj result (first nums1)) (rest nums1) nums2)
+              (recur (conj result (first nums2)) nums1 (rest nums2))))))
+
+(deftest merge-test
+  (testing "basic"
+    (is (= [1 2 2 3 5 6] (merge [1 2 3] [2 5 6])))
+    (is (= [1] (merge [1] [])))
+    (is (= [1] (merge [] [1])))
+    (is (= [1 2 3 4 5 6] (merge [1 3 4 5] [2 6])))
+    (is (= [] (merge [] [])))))
