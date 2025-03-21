da203d851a0bd14faffe8be6c3ebc74e2c108bed
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-03-20 21:19:08 -0300

    clojure: Length of Last Word

---
+++ clojure/algorithms/others/length-of-last-word-mar-20-2025/main.clj
@@ -0,0 +1,45 @@
+(ns mini.playground
+  (:require
+   [clojure.string :as string]
+   [clojure.test :refer [deftest is testing]]))
+
+;; Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+;; A word is a maximal substring consisting of non-space characters only.
+
+;; Example 1:
+
+;; Input: s = "Hello World"
+;; Output: 5
+;; Explanation: The last word is "World" with length 5.
+;; Example 2:
+
+;; Input: s = "   fly me   to   the moon  "
+;; Output: 4
+;; Explanation: The last word is "moon" with length 4.
+;; Example 3:
+
+;; Input: s = "luffy is still joyboy"
+;; Output: 6
+;; Explanation: The last word is "joyboy" with length 6.
+
+;; Constraints:
+
+;; 1 <= s.length <= 104
+;; s consists of only English letters and spaces ' '.
+;; There will be at least one word in s.
+
+;; time O(n)
+;; space O(n)
+(defn length-of-last-word [s]
+  (-> (string/split s #"\s+")
+      last
+      count))
+
+(deftest length-of-last-word-test
+  (testing "basic"
+    (is (= 5 (length-of-last-word "Hello World")))
+    (is (= 4 (length-of-last-word "   fly me   to   the moon  ")))
+    (is (= 6 (length-of-last-word "luffy is still joyboy")))))
+
+(count (last (string/split "a b  c" #"\s+")))
